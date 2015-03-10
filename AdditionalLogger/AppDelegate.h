397b85137ac7842fe71cf59cd94f8c8f31e4dd23
lixianliang <lixianliang@corp.netease.com>
2015-03-10 16:11:52 +0800

    Initial Commit

---
+++ AdditionalLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdditionalLogger
+//
+//  Created by oldman on 3/10/15.
+//  Copyright (c) 2015 oldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdditionalLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdditionalLogger
+//
+//  Created by oldman on 3/10/15.
+//  Copyright (c) 2015 oldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
